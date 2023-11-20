5d976818d3761de6366a60e2af2ae7f2da291df7
S0mbr4xc <s3._xc@hotmail.com>
2023-11-20 08:17:15 -0500

    Creacion del menú

--- Prueba_1/src/app/app-routing.module.ts
+++ Prueba_1/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { DatosComponent } from './pages/datos/datos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "paginas/agregar", component: AgregarComponent},
+  {path: "paginas/datos", component: DatosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Prueba_1/src/app/app.module.ts
+++ Prueba_1/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { DatosComponent } from './pages/datos/datos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AgregarComponent,
+    DatosComponent
   ],
   imports: [
     BrowserModule,
